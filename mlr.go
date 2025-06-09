00649ed139bdb340bfbe024f23ddf3c8f57d7400
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-06-09 09:36:13 +0900

    Add documentation comments for mlrMDInplacePut function and command-line flags

--- mlr.go
+++ mlr.go
@@ -6,14 +6,20 @@ import (
 	mlrentry "github.com/johnkerl/miller/v6/pkg/entrypoint"
 )
 
+// mlrMDInplacePut runs Miller with the specified file path and script for processing.
+// filePath is the path to the input file. Miller, as a library, does not support processing data in memory.
 func mlrMDInplacePut(filePath string, script string) {
 	argsSave := os.Args
 	defer func() { os.Args = argsSave }()
 	os.Args = []string{
 		"mlr",
+		// List of command-line flags - Miller Documentation https://miller.readthedocs.io/en/latest/reference-main-flag-list/
+		// File formats - Miller Documentation https://miller.readthedocs.io/en/latest/file-formats/
 		"--imarkdown",
 		"--omarkdown",
+		// In-place mode - Miller Documentation https://miller.readthedocs.io/en/latest/reference-main-in-place-processing/
 		"-I",
+		// List of verbs - Miller Documentation https://miller.readthedocs.io/en/latest/reference-verbs/#put
 		"put",
 		"-e", script,
 		filePath,
